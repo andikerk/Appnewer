6adfd20040cfb9a29e49fb78efa90386981ea1aa
Andreas Kerkhoff <akerkhoff@gmx.de>
2017-07-12 11:23:38 +0200

    removed parentheses after ProductChannel.broadcast_to

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -3,13 +3,15 @@ class CommentUpdateJob < ApplicationJob
 
   def perform(comment, current_user)
     # Do something later
-     ProductChannel.broadcast_to (comment.product.id, comment: render_comment(comment, current_user),
-       average_rating: comment.product.average_rating)
+     ProductChannel.broadcast_to comment.product.id, 
+     comment: render_comment(comment, current_user),
+     average_rating: comment.product.average_rating
   end
 
   private
   def render_comment (comment, current_user)
-  	 CommentsController.render (partial: 'comments/comment', locals: {comment: comment, current_user: current_user})
+  	 CommentsController.render (partial: 'comments/comment', 
+  	 locals: {comment: comment, current_user: current_user})
 
   end
 end
